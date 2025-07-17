e7f486b14fb0008bbca6b867e32e115419bf59ab
Sanlexis95 <alexis1clerge@gmail.com>
2025-07-17 06:30:53 +0200

    Update db.py

--- app/db.py
+++ app/db.py
@@ -1,9 +1,11 @@
 import mysql.connector
+import os
 
 def get_connection():
     return mysql.connector.connect(
-        host="votre_host",
-        user="votre_user",
-        password="votre_mot_de_passe",
-        database="votre_bdd"
+        host="49.12.169.210",
+        port=3306,
+        user=os.getenv("DB_USER"),          # à définir dans Streamlit Cloud
+        password=os.getenv("DB_PASS"),
+        database=os.getenv("DB_NAME")
     )
