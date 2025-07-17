1bab25d057bafda3d647d294caa056e1c65e4fd3
Sanlexis95 <alexis1clerge@gmail.com>
2025-07-17 06:33:33 +0200

    Update db.py

--- app/db.py
+++ app/db.py
@@ -5,7 +5,7 @@ def get_connection():
     return mysql.connector.connect(
         host="49.12.169.210",
         port=3306,
-        user=os.getenv("DB_USER"),          # à définir dans Streamlit Cloud
-        password=os.getenv("DB_PASS"),
-        database=os.getenv("DB_NAME")
+        user=os.getenv("DB_PORT", 3306"),          # à définir dans Streamlit Cloud
+        password=os.getenv("DFKyRfGvhaKkywkB"),
+        database=os.getenv("nexus_db_9374")
     )
